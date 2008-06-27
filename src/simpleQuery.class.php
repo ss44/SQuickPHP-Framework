df25a4535d2b08271f86bece840a9383ff7f0583
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2008-06-27 00:21:35 +0000

    SimpleQuery Initial Base
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@2 0aba5d34-43d7-11dd-bbfb-1710253988b0

---
+++ src/simpleQuery.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Assists in building general sql queries in an organized manner.
+ * No more messing with 20 line strings, simply use this to build a query in a simple
+ * organzing and readable method, without loosing any flexiblity of raw sql.
+ * 
+ * @author Shajinder Padda <shajinder@gmail.com>
+ * @created 26-June-2008
+ */
+
+class SimpleQuery{
+	
+	public function addColumn($columnName);
+	
+	public function addColumns($columns);
+	
+	public function addField($fieldName, $value);
+	
+	public function addFields( $fields );
+	
+	public function addTable( $table );
+	
+	public function addLeftJoin( $table, $onClause);
+	
+	public function addRightJoin( $table, $onClause);
+	
+	public function addJoin( $table, $onClause);
+	
+	public function addWhere( $field, $value = '', $operator = '');
+	
+	public function getSelect();
+	
+	public function getInsert();
+	
+	public function getUpdate();
+	
+}
+?>
---
+++ src/test/simpleQuery.test.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Test page to test functionality of simpleQuery.
+ */
+require_once('../simpleQuery.class.php');
+
+$tests = array();
+
+$q = new SimpleQuery();
+
+//1. Test Building SELECT statements
+
+$expected = "SELECT * FROM test";
+$tests[] = $q->getSelect() == $expected;
+
+
+$expected = "SELECT a, b, c FROM test";
+$tests[] = $q->getSelect() == $expected;
+
+
+$expected = "SELECT a, DISTINCT(b), COUNT(c) FROM test, test2 WHERE (a = 5) AND (b = 'This\'s a String')";
+$tests[] = $q->getSelect() == $expected;
+
+foreach ($tests as $testNo=>$test){
+	echo !$test ? 'Failed Test: '. ($testNo+1) . "\n" : '';
+}
+
+?>
