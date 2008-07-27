23972061f17cf12f1ead63b4cb91f14300e41c6c
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2008-07-27 21:17:37 +0000

    updates
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@17 0aba5d34-43d7-11dd-bbfb-1710253988b0

--- src/simpleScrape.php
+++ src/simpleScrape.php
@@ -11,22 +11,32 @@ class simpleScrape{
 	
 	protected $uri = '';
 	protected $method = '';
+	protected $host = '';
+	protected $port = 80;
 	
 	public function __construct( $uri, $method = "GET" ){
-			$this->uri = $uri;
+			$this->parseUri($uri);
 			$this->method = $method;
 	}
 	
 	public function setUserAgent( $userAgent ){
 		
 	}
-	
+		
 	public function setOS( $os ){
 		
 	}
 	
 	public function getPage(){
-		return file_get_contents($this->uri);
+		$fp = fsockopen($this->host, 80);
+		//return file_get_contents($this->uri);
+	}
+	
+	protected function parseUri( $uri ){
+		oops($uri);
+		preg_match('/(^https?:\/\/)?([a-z0-9\.\-\/])(\?.*)?$/i', $uri, $temp);
+		oops($uri);
+		oops($temp);
 	}
 }
 ?>
