5a57054a00b7ed3ea6a9160a0f43ab79f3c7ed9d
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2009-01-12 15:27:05 +0000

    renamed functions to simpleFunctions.php
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@21 0aba5d34-43d7-11dd-bbfb-1710253988b0

--- src/simpleFunctions.php
+++ src/simpleFunctions.php
@@ -40,19 +40,49 @@ function dim($var, $showVarDump = false){
  * @param mixed $agr2 If applied will be the max value to compare against. For strings if both values are ints will take it
  * to mean the 
  */
-function qValidate($var, $type = 'str', $arg1 = null, $agr2 = null){
-
+function cleanVar($var, $type = 'str', $arg1 = null, $arg2 = null){
+	$checks = false;
+	
 	switch ($type){
 		case 'str':
-			if (!is_a($var, 'String')) return false;
-			
-			
+			if (!is_a($var, 'String')) return null;
 			break;
 		case 'float':
 		case 'double':
 		case 'int':
+			if (!is_numeric($var)) return null;
+			if (!is_null($arg1) && $var < $arg1) return null;
+			if (!is_null($arg2) && $var > $arg2) return null;
+			return (int) $var;
 		case 'bool':
-	
 	}
 }
+
+/**
+ * Convience function that passes a variable to cleanVar which validates a given value. This method is specific
+ * to REQUEST vars where you pass the post field name instead of the var, the rest of the arguments follow
+ * the same structure as cleanVar
+ *
+ * @param string $field Field name from the post which you want to clean
+ * @param See Param lsit of cleanVar from the second argument on for the rest.
+ * @return mixed Returns the variable cleaned or null if not loaded.
+ */
+function cleanREQUEST($field){
+	
+	$args = func_get_args();
+
+	$type = isset($args[1]) ? $args[1] : null;
+	$arg1 = isset($args[2]) ? $args[2] : null;
+	$arg2 = isset($args[3]) ? $args[3] : null;
+
+	if (array_key_exists($field, $_REQUEST)){
+		return cleanVar( $_REQUEST[$field], $type, $arg1, $arg2); 
+	}
+	
+	return null;
+	
+}
+
+
+
 ?>
