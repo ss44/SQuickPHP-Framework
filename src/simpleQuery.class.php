310840910f447cba161a1778b4f8d7a9b62b7e3f
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2009-02-21 17:57:32 +0000

    fixed some deprecated functions
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@29 0aba5d34-43d7-11dd-bbfb-1710253988b0

--- src/simpleQuery.class.php
+++ src/simpleQuery.class.php
@@ -244,7 +244,7 @@ class SimpleQuery{
 				//TODO do a type check using typeof() to determine if is a real int instead checking if is numeric
 				$str .= $where['field'] . ' IN (\'' . implode("', '", $where['value'] ) . '\')';
 			}
-			elseif (is_a($where['value'], 'simpleQuery')){
+			elseif ($where['value'] instanceof simpleQuery){
 				$obj = $where['value'];
 				$str .= $where['field'] . ' IN (' . $where['value']->getSelect(). ')';
 			}
