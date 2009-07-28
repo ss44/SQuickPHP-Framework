bb12c86779ba547df63981b6327ce979c0d20f58
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2009-07-28 11:41:55 +0000

    started coding simpleID3
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@35 0aba5d34-43d7-11dd-bbfb-1710253988b0

---
+++ src/simpleID3.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Reads and parses ID3 Tags from MP3 Files.
+ *
+ * @author Shajinder Padda <shajinder@gmail.com>
+ * @created 28-July-2009
+ */
+
+class simpleID3{
+	
+	protected $_filename;
+	protected $_id3Array;
+	protected $_refresh;
+	
+	public function __constructor( $filename ){
+		
+	}
+	
+	public function getArray( $filename ){
+		
+	}
+	
+	protected function parseID3(){
+		
+	}
+	
+	protected loadFile( $filename ){
+		
+	}
+	
+	/**
+	 * Reads and returns an array of all id3 information found in a given file.
+	 * 
+	 * @param String $fileName Path to the file to try and parse id3 tag information from.
+	 */
+	public static function getID3Array ( $fileName ){
+		
+	}
+}
+ 
+
+?>
