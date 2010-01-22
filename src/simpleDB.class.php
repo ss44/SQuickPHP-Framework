7eb5f1a3a62eb3890f6ef02f98b573823a0b4a39
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2010-01-22 21:47:55 +0000

    - added exceptions
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@40 0aba5d34-43d7-11dd-bbfb-1710253988b0

--- src/simpleDB.class.php
+++ src/simpleDB.class.php
@@ -3,24 +3,24 @@
  * Simple DB is a custom abstraction layer to communicate with various DB classes
  * Plans are to support
  * mysql, mysqli, postGres, and sqlLite.
- * 
- * @author Shajinder Padda <shajinder@gmail.com> 
+ *
+ * @author Shajinder Padda <shajinder@gmail.com>
  * @created 13-Dec-2008
  */
 
 //require_once('simpleQuery.class.php');
 
 class SimpleDB{
-	
+
 	protected $_dbType = '';
 	protected $_dbName = '';
 	protected $_dbUser = '';
 	protected $_dbPass = '';
 	protected $_dbPath = '';
 	protected $connection = null;
-	
+
 	public function __construct( $_CONFIG = null){
-		
+
 		if (is_array($_CONFIG)){
 			//Try to load settings from array
 			$this->_dbType = array_key_exists('type', $_CONFIG) ? $_CONFIG['type'] : null;
@@ -28,22 +28,22 @@ class SimpleDB{
 			$this->_dbName = array_key_exists('name', $_CONFIG) ? $_CONFIG['name'] : null;
 			$this->_dbUser = array_key_exists('user', $_CONFIG) ? $_CONFIG['user'] : null;
 			$this->_dbPass = array_key_exists('pass', $_CONFIG) ? $_CONFIG['pass'] : null;
-			
+				
 		}elseif(file_exists('site.ini') || (defined(SIMPLE_INI_FILE) && file_exists(SIMPLE_INI_FILE))){
 			//If not found then check settings from config file
-                        $siteIni = defined(SIMPLE_INI_FILE) ? SIMPLE_INI_FILE : 'site.ini';
+			$siteIni = defined(SIMPLE_INI_FILE) ? SIMPLE_INI_FILE : 'site.ini';
 
-                        $config =  parse_ini_file( $siteIni );
-			
+			$config =  parse_ini_file( $siteIni );
+				
 			if (array_key_exists('DB_TYPE', $config)) $this->_dbType = $config['DB_TYPE'];
 			if (array_key_exists('DB_PATH', $config)) $this->_dbPath = $config['DB_PATH'];
 			if (array_key_exists('DB_NAME', $config)) $this->_dbName = $config['DB_NAME'];
 			if (array_key_exists('DB_USER', $config)) $this->_dbUser = $config['DB_USER'];
-			if (array_key_exists('DB_PASS', $config)) $this->_dbPass = $config['DB_PASS'];			
+			if (array_key_exists('DB_PASS', $config)) $this->_dbPass = $config['DB_PASS'];
 		}else{
-			throw Exception('No db settings provided.');
+			throw SimpleDBException('No db settings provided.');
 		}
-		
+
 		//@TODO: Try loading db if unable to load throw an exception.
 		switch ($this->_dbType){
 			case 'mysql':
@@ -54,15 +54,15 @@ class SimpleDB{
 			case 'postgres':
 			case 'sqlite':
 			default:
-				throw new Exception('Invalid/Unsupported database type.');
+				throw new SimpleDBException('Invalid/Unsupported database type.');
 		}
-		
-		if (!$this->connection) throw new Exception("Unable to connect to DB.");
+
+		if (!$this->connection) throw new SimpleDBException("Unable to connect to DB.");
 	}
-	
+
 	/**
 	 * Executes an insert command.
-	 * 
+	 *
 	 * @param SimpleQuery $q Query object that contains the insert statement.
 	 * @return int Returns the last insert id if available or null if not.
 	 * @TODO: Set the insert_id in the simpleQuery object.
@@ -79,10 +79,10 @@ class SimpleDB{
 			case 'sqlite':
 		}
 	}
-	
+
 	/**
 	 * Executes an update command from simple query.
-	 * 
+	 *
 	 * @param SimpleQuery $q Query object that contains the update statement.
 	 */
 	public function update(SimpleQuery $q){
@@ -95,17 +95,17 @@ class SimpleDB{
 			case 'sqlite':
 		}
 	}
-	
+
 	/**
 	 * Executes the delete command from a simple query object.
-	 * 
+	 *
 	 * @param SimpleQuery $q Query object that contains the delete statement.
 	 * @param bool $overRide If no where statements are detected, will throw an error unless override is set to true.
 	 * 	This is just added protection from accidently deleting all rows in a table.
 	 */
 	public function delete(simpleQuery $q, $overRide = false){
-		if ( !$overRide && empty($q->wheres) ) throw new Exception ("No where set for delete. Must set override to continue.");
-		
+		if ( !$overRide && empty($q->wheres) ) throw new SimpleDBException ("No where set for delete. Must set override to continue.");
+
 		switch ($this->_dbType){
 			case 'mysql':
 				$result = mysql_query( $q->getDelete(), $this->connection);
@@ -115,31 +115,31 @@ class SimpleDB{
 			case 'sqlite':
 		}
 	}
-	
+
 	/**
 	 * Returns the table structure for a given table as an associtive array.
-	 * 
+	 *
 	 * @param String $tableName Name of the table who's structure you are trying to retrieve.
-	 * @return array A 2D associative of the fields with field properties. 
+	 * @return array A 2D associative of the fields with field properties.
 	 */
 	public function getTableStructure($tableName){
 		$result = array();
 
 		switch ($this->_dbType){
 			case 'mysql':
-				$r = mysql_query('Describe '.mysql_real_escape_string($tableName), $this->connection);				
+				$r = mysql_query('Describe '.mysql_real_escape_string($tableName), $this->connection);
 				while ($r == true && $row = mysql_fetch_assoc($r)){
 					$result[ $row['Field'] ] = $row;
 				}
-				
+
 				return $result;
 				break;
 			case 'mysqli':
 			case 'postgres':
 			case 'sqlite':
-		}	
+		}
 	}
-	
+
 	/**
 	 * Returns a single row from the current result set.
 	 *
@@ -147,33 +147,33 @@ class SimpleDB{
 	 */
 	public function getRow(simpleQuery $q){
 		$result = array();
-		
+
 		switch ($this->_dbType){
 			case 'mysql':
 				$r = mysql_query($q->getSelect(), $this->connection);
-				
+
 				if (@mysql_num_rows($r) > 0){
 					$result = mysql_fetch_assoc( $r );
 					return $result;
 				}
-				
+
 				break;
 			case 'mysqli':
 			case 'postgres':
-			case 'sqlite':	
+			case 'sqlite':
 		}
-		
+
 		return $result;
 	}
-	
+
 	/**
 	 * Returns all results from the current result set.
-	 * 
+	 *
 	 * @param SimpleQuery $q Query object that contains the update statement.
 	 */
 	public function getAll(SimpleQuery $q ){
 		$result = array();
-		
+
 		switch ($this->_dbType){
 			case 'mysql':
 				$r = mysql_query($q->getSelect(), $this->connection);
@@ -187,55 +187,55 @@ class SimpleDB{
 			case 'mysqli':
 			case 'postgres':
 			case 'sqlite':
-			
+					
 		}
 		return $result;
 	}
-	
-        /**
-         * Gets the column from a result set.
-         * 
-         * @param SimpleQuery $q The simple query object that we want to iterate.
-         * @param String $column The column that we want to retrieve.
-         */
-        public function getColumn( SimpleQuery $q, $column ){
-            $q->addColumn( $column );
-            
-            $result = array();
 
-            switch ($this->_dbType){
-                case 'mysql':
-                    $r = mysql_query($q->getSelect(), $this->connection);
+	/**
+	 * Gets the column from a result set.
+	 *
+	 * @param SimpleQuery $q The simple query object that we want to iterate.
+	 * @param String $column The column that we want to retrieve.
+	 */
+	public function getColumn( SimpleQuery $q, $column ){
+		$q->addColumn( $column );
 
-                    if (mysql_num_rows($r) > 0){
-                        while ($row = mysql_fetch_assoc($r)){
-                            $result[] = $row[ $column ];
-                        }
-                    }
+		$result = array();
 
-                    return $result;
-            }
-        }
+		switch ($this->_dbType){
+			case 'mysql':
+				$r = mysql_query($q->getSelect(), $this->connection);
+
+				if (mysql_num_rows($r) > 0){
+					while ($row = mysql_fetch_assoc($r)){
+						$result[] = $row[ $column ];
+					}
+				}
+
+				return $result;
+		}
+	}
 	/**
 	 * Returns an associative array from the current result set.
-	 * 
+	 *
 	 * @param SimpleQuery $q Query object tthat contains the update statement.
 	 * @param string $key The column that should be the key for the array.
 	 * @param string $value The column that will be the value of the associtive array.
 	 */
 	public function getAssoc(SimpleQuery $q, $key, $value = null){
 		$result = array();
-		
+
 		switch ($this->_dbType){
 			case 'mysql':
 				$r = mysql_query($q->getSelect(), $this->connection);
-				
+
 				if (mysql_num_rows($r) > 0){
 					while( $row = mysql_fetch_assoc($r)){
 						if (array_key_exists($key, $row)){
-							$result[ $row[$key] ] = $value && array_key_exists($value, $row) ? $row[$value] : $row;  
+							$result[ $row[$key] ] = $value && array_key_exists($value, $row) ? $row[$value] : $row;
 						}else{
-							throw new Exception('Invalid key. Not found in result.');
+							throw new SimpleDBException('Invalid key. Not found in result.');
 						}
 					}
 					return $result;
@@ -247,5 +247,10 @@ class SimpleDB{
 		}
 		return $result;
 	}
-	
+}
+
+class SimpleDBException extends Exception{
+	public function __construct( $message = null){
+		parent::__construct( $message );
+	}
 }
--- src/simpleQuery.class.php
+++ src/simpleQuery.class.php
@@ -3,33 +3,33 @@
  * Assists in building general sql queries in an organized manner.
  * No more messing with 20 line strings, simply use this to build a query in a simple
  * organzing and readable method, without loosing any flexiblity of raw sql.
- * 
+ *
  * @author Shajinder Padda <shajinder@gmail.com>
  * @created 26-June-2008
  */
 
 class SimpleQuery{
-	
+
 	public $tables = array();
 	public $columns = array();
 	public $fields = array();
 	public $joins = array();
 	public $wheres = array();
-	public $groups = array(); 
+	public $groups = array();
 	public $havings = array();
-    public $orders = array();
+	public $orders = array();
 	public $whereGroups = array();
 	public $whereGroupCounter = 0;
 	protected $limit = null;
-	
+
 	public function __construct(){
 		$this->whereGroups[0] = 'AND';
 	}
-	
+
 	public function addColumn($columnName){
 		$this->columns[] = $columnName;
 	}
-	
+
 	public function addColumns($columns){
 		if (is_array($columns)){
 			$this->columns = array_merge($this->columns, $columns);
@@ -37,7 +37,7 @@ class SimpleQuery{
 			$this->addColumn($columns);
 		}
 	}
-	
+
 	public function addField($fieldName, $value = '', $escape = true){
 		$pair = array();
 		$pair['field'] = $fieldName;
@@ -45,23 +45,23 @@ class SimpleQuery{
 		$pair['escape'] = (bool) $escape;
 		$this->fields[] = $pair;
 	}
-	
+
 	public function addFields( $fields ){
 		if (is_array($fields)){
 			foreach ($fields as $field=>$value){
 				$pair = array();
 				$pair['field'] = $field;
 				$pair['value'] = $value;
-				
+
 				$this->fields[] = $pair;
 			}
 		}
 	}
-	
+
 	public function addTable( $table ){
 		$this->tables[] = $table;
 	}
-	
+
 	public function addLeftJoin( $table, $onClause = ''){
 		$pair = array();
 		$pair['table'] = $table;
@@ -69,7 +69,7 @@ class SimpleQuery{
 		$pair['type'] = 'left';
 		$this->joins[] = $pair;
 	}
-	
+
 	public function addRightJoin( $table, $onClause){
 		$pair = array();
 		$pair['table'] = $table;
@@ -77,7 +77,7 @@ class SimpleQuery{
 		$pair['type'] = 'right';
 		$this->joins[] = $pair;
 	}
-	
+
 	public function addJoin( $table, $onClause){
 		$pair = array();
 		$pair['table'] = $table;
@@ -85,7 +85,7 @@ class SimpleQuery{
 		$pair['type'] = 'inner';
 		$this->joins[] = $pair;
 	}
-	
+
 	public function addWhere( $field, $value = '', $operator = '='){
 		$pair = array();
 		$pair['field'] = $field;
@@ -94,22 +94,22 @@ class SimpleQuery{
 		$pair['group'] = $this->whereGroupCounter;
 		$this->wheres[] = $pair;
 	}
-	
+
 	public function startWhereGroup( $type = 'OR' ){
 		$this->whereGroupCounter++;
 		$this->whereGroups[$this->whereGroupCounter] = $type;
 	}
-	
+
 	public function endWhereGroup(){
 		$this->whereGroupCounter = $this->whereGroupCounter == 0 ? 0 : $this->whereGroupCounter-1;
 	}
-	
+
 	public function addGroup($group){
 		$this->groups[] = $group;
 	}
-	
+
 	public function addGroups($groups){}
-	
+
 	public function addHaving($field, $value, $operator = '='){
 		$pair = array();
 		$pair['field'] = $field;
@@ -117,142 +117,142 @@ class SimpleQuery{
 		$pair['operator'] = $operator;
 		$this->havings[] = $pair;
 	}
-	
+
 	public function addHavings($fields){}
 
-        /**
-         * The order by fields to add to the select statement.
-         *
-         * @param String $field Field you want to sort by.
-         * @param Direction $direction  Direction to sort by, should be either ASC or DESC.
-         */
+	/**
+	 * The order by fields to add to the select statement.
+	 *
+	 * @param String $field Field you want to sort by.
+	 * @param Direction $direction  Direction to sort by, should be either ASC or DESC.
+	 */
 	public function addOrderBy($field, $direction = "ASC"){
-            $order = arraY();
-            $order['field'] = $field;
-            $order['direction'] = $direction;
-            $this->orders[] = $order;
-        }
-	
+		$order = arraY();
+		$order['field'] = $field;
+		$order['direction'] = $direction;
+		$this->orders[] = $order;
+	}
+
 	public function addLimit( $value ){
 		$this->limit = (is_int($value)) ? $value : null;
 	}
-	
+
 	public function getSelect(){
 		$str = 'SELECT ';
-		
+
 		if (!$this->columns) $str .= '* ';
 		else $str .= $this->prepareColumns();
-		
+
 		if (!$this->tables) return false;
 		else $str .= $this->prepareTables();
-		
+
 		if ($this->joins) $str .= ' '.$this->prepareJoins();
 
 		if ($this->wheres) $str .= ' '.$this->prepareWhere();
-		
+
 		if ($this->groups) $str .= ' '.$this->prepareGroup();
 		if ($this->havings) $str .= ' '.$this->prepareHaving();
 		if ($this->orders) $str .= ' ' . $this->prepareOrders();
-                
-		if (!is_null($this->limit)) $str .= ' LIMIT '.$this->limit; 
-		
+
+		if (!is_null($this->limit)) $str .= ' LIMIT '.$this->limit;
+
 		return trim($str);
 	}
-	
+
 	public function getInsert(){
-                if (!$this->tables) throw new Exception('Must set a table first.');
+		if (!$this->tables) throw new Exception('Must set a table first.');
 		if (!$this->fields) throw new Exception('No fields set');
-		
+
 		$str = 'INSERT INTO ';
-		
+
 		//Can only insert into one table at a time so grab the last table we tried to insert
 		$lastTable = end($this->tables);
 		$str .= $lastTable;
-		
+
 		$fields = '';
 		$values = '';
-		
+
 		$counter = count($this->fields);
 		$x = 0;
 
-                foreach ($this->fields as $pair){
+		foreach ($this->fields as $pair){
 			$x++;
 			$field = $pair['field'];
 			$value = $pair['value'];
-			
+				
 			$fields .= '`'.mysql_real_escape_string($field).'`';
-			
+				
 			if ($pair['escape'])
-				$values .= (is_numeric($value) || is_bool($value)) ? $value : '\''.mysql_real_escape_string($value).'\'';
+			$values .= (is_numeric($value) || is_bool($value)) ? $value : '\''.mysql_real_escape_string($value).'\'';
 			else
-				$values .= $value;
+			$values .= $value;
 			/*
-			if (is_numeric($value))
+			 if (is_numeric($value))
 				$values .= $value;
-			elseif (is_bool($value))
+				elseif (is_bool($value))
 				$values .= $value === true ? 1 : 0;
-			else 
+				else
 				$values .= '\''.mysql_real_escape_string($value).'\'';
-			*/
-			if ($x < $counter){ 
+				*/
+			if ($x < $counter){
 				$fields .= ', ';
 				$values .= ', ';
 			}
 		}
-		
+
 		$str .= "($fields) VALUES($values)";
 		return $str;
 	}
-	
+
 	public function getUpdate(){
 		if (!$this->tables) throw new Exception("Must set a table.");
 		if (!$this->fields) throw new Exception("Must select fields.");
-		
+
 		$str = 'UPDATE ';
 		$str .= join(', ', $this->tables);
-		
+
 		if ($this->joins) $str .= ' '.$this->prepareJoins();
-		
+
 		$str .= ' SET ';
-		
+
 		$count = count($this->fields);
 		$x = 0;
-		
+
 		foreach($this->fields as $pair){
 			$x++;
-			
+				
 			$field = $pair['field'];
 			$value = $pair['value'];
-			
+				
 			if (!$value)  $str .= "$field = NULL";
 			elseif (is_numeric($value)) $str .= $field.'='.$value;
 			else $str .= $field.'='.'\''.mysql_real_escape_string($value).'\'';
-			
+				
 			if ($x < $count){
 				$str .= ', ';
 			}
 		}
-		
+
 		if ($this->wheres) $str .= ' '.$this->prepareWhere();
-		
+
 		return $str;
 	}
-	
+
 	public function getDelete(){
 		if (!$this->tables) throw new Exception("Must set a table.");
-		
+
 		$str = 'DELETE FROM ';
 		$str .= join(', ', $this->tables);
-		
+
 		if ($this->wheres) $str .= ' '.$this->prepareWhere();
-		
+
 		return $str;
 	}
-	
+
 	public function prepareColumns(){
 		return join(', ', $this->columns).' ';
 	}
-	
+
 	protected function prepareWhere(){
 		$str = 'WHERE ';
 		$boolType = 'AND';
@@ -269,7 +269,7 @@ class SimpleQuery{
 				$endedGroup = false;
 			}elseif($where['group'] < $currentGroup){
 				$str .= ')';
-				$boolType = $this->whereGroups[ $where['group']]; 
+				$boolType = $this->whereGroups[ $where['group']];
 				$currentGroup = $where['group'];
 				$endedGroup = true;
 			}
@@ -287,19 +287,19 @@ class SimpleQuery{
 			else $str .= $where['field'] . ' ' . $where['operator'] . " '" . mysql_real_escape_string($where['value'])."'";
 			$counter++;
 			if ($counter != $numberOfItems){
-				$str .= " $boolType ";		
+				$str .= " $boolType ";
 			}
 		}
-		
+
 		if (!$endedGroup) $str .= ')';
-		
+
 		return $str;
 	}
-	
+
 	protected function prepareTables(){
 		return 'FROM '.join(', ', $this->tables);
 	}
-	
+
 	protected function prepareJoins(){
 		$str = '';
 
@@ -319,24 +319,24 @@ class SimpleQuery{
 					$str .= 'INNER ';
 					break;
 			}
-			
+				
 			$str .= 'JOIN '. $join['table']. ' ON (' .$join['on'] . ') ';
 
 		}
-		
+
 		return trim($str);
 	}
-	
+
 	protected function prepareGroup(){
 		return 'GROUP BY '. join(', ', $this->groups);
-		
+
 	}
-	
+
 	protected function prepareHaving(){
 		$str = 'HAVING ';
 		$numberOfItems = count($this->havings);
 		$counter = 0;
-		
+
 		foreach ($this->havings as $having){
 			$str .= $having['field'] . $having['operator'] . (is_numeric($having['value']) ? $having['value'] : "'" . mysql_escape_string($having['value']) . "'") ;
 			$counter ++;
@@ -345,20 +345,26 @@ class SimpleQuery{
 		return $str;
 	}
 
-        protected function prepareOrders(){
-            $str = 'ORDER BY ';
-            $numberOfItems = count($this->orders);
-            $counter = 0;
+	protected function prepareOrders(){
+		$str = 'ORDER BY ';
+		$numberOfItems = count($this->orders);
+		$counter = 0;
 
-            foreach ($this->orders as $order){
-                $str .= "$order[field] $order[direction]";
-                $counter++;
-                if ( $numberOfItems != $counter) $str .= ', ';
-            }
-            
-            return $str;
-        }
+		foreach ($this->orders as $order){
+			$str .= "$order[field] $order[direction]";
+			$counter++;
+			if ( $numberOfItems != $counter) $str .= ', ';
+		}
+
+		return $str;
+	}
 
 	protected function prepareFields(){}
+}
+
+class SimpleQueryException extends Exception{
+	public function __construct( $message = null ){
+		parent::__construct( $message );	
+	}
 }
 ?>
