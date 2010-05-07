fe21e30c9d3de4cabfc408b858138e3eee4b5dbc
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2010-05-07 04:19:46 +0000

    started drawing out simpleFormField class
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@42 0aba5d34-43d7-11dd-bbfb-1710253988b0

---
+++ src/simpleFormField.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ * 
+ * @author Shajinder Padda <shajinder@gmail.com>
+ * @created Apr 30, 2010
+ */
+?>
