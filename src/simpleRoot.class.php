46828155d74418b5a010c828091cebec62672c18
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2010-11-13 19:12:23 +0000

    started coding simpleRoot to handle loading of basic objects from the database
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@44 0aba5d34-43d7-11dd-bbfb-1710253988b0

---
+++ src/simpleRoot.class.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * A base class to load a table as an object and validate fields,
+ * against the table structure.
+ * 
+ * @author Shajinder Singh <ss@ss44ca>
+ * @created 13-Nov-2010
+ */
+
+class SimpleRoot extends SimpleDB{
+	
+	protected $_table = null;
+	protected $_primaryKey = null;
+	protected $_tableInfo = null;
+	protected $_data = null;
+	protected $_isNew = true;
+	
+	public function __construct( $keyId = null ){
+		if ( $keyId ){
+			
+		}
+	}
+	
+	public function load( $loadParams ){
+		
+		$q = new uQuery();
+		$q->addTable( $this->_table );
+		$q->addWhere( $loadParams );
+		
+		$this->_data = $this->getRow( $q );
+	}
+	
+	public function save( $saveParams ){
+		
+		$q = new uQuery();
+		$q->addTable( $this->_table );
+		
+		
+		//If we have a primary key then update otherwise insert
+		if ($this->_isNew){
+			$this->insert($q);
+		}else{
+			$this->update($q);	
+		}
+		
+		
+	}
+	
+	public function __set( $key, $value ){
+		if (!array_key_exists( $key, $this->_data )) die("Invalid $key. Does not exist in data");
+		if (!array_key_exists( $key, $this->_tableInfo )) die("Invalid $key. Does not exist in data");
+		
+		$cleanValue = null;
+		
+		$this->_data[ $key ] = $cleanValue;
+	}
+	
+	public function __get( $key ){
+		if (!array_key_exists( $key, $this->_data )) die("Invalid $key. Does not exist in data");
+		
+		return $this->_data[ $key ];
+	}
+}
