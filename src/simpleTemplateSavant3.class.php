d3896195c8122e4bc8831953ec6a7953cc8749e0
shinda <shinda@0aba5d34-43d7-11dd-bbfb-1710253988b0>
2011-10-08 20:07:04 +0000

    added template wrapper class to be used with Savant3
    git-svn-id: svn+ssh://home.ss44.ca/simpleCode/trunk@64 0aba5d34-43d7-11dd-bbfb-1710253988b0

---
+++ src/simpleTemplateSavant3.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * An extended form of Savant3 used in particularly to support use of a wapper.
+ * 
+ * @author Shajinder Singh <ss@ss44.ca>
+ * @created 10-Oct-2011
+ */
+
+ if (!class_exists('Savant3')){
+ 	throw new SimpleException("Cannot use SimpleTemplateSavant3 without first including the Savant3 template engine.", 1);
+ }
+ 
+ class SimpleTemplateSavant3 extends Savant3{
+ 	
+	/**
+	 * String name of the wrapper file to use.
+	 */
+	protected $_wrapper = null;
+	
+	/**
+	 * Sets the wrapper file to use.
+	 * 
+	 * @param String Wrapper  
+	 */
+	public function setWrapper( $wrapperFile ){
+		$this->_wrapper = $wrapperFile;
+	}
+	
+	
+	public function getWrapper(){
+		return $this->_wrapper;
+	}
+	
+	/**
+	 * @override The parent display method to try and call the wrapper.
+	 * 
+	 * @param $tpl String File / Template to load.
+	 * @author $file File to attempt to load.
+	 */
+	public function display( $tpl = null ){
+		
+		
+		try{
+			if ( $this->getWrapper() ){
+				$value = $this->fetch( $tpl ) ;
+				$this->_content = $value;
+				parent::display( $this->getWrapper() );
+			}
+		}catch( Exception $e ){
+			die( $e->getMessage() );
+		}
+		
+	}
+	
+ }
