4917d13cafb6827675c94560870be6cdfe42eed9
SSingh <ss@ss44.ca>
2013-09-22 03:51:09 -0400

    abstracted data access functionality so to create data objects not tied to the db

--- RootDB.class.php
+++ RootDB.class.php
@@ -9,12 +9,11 @@
 
 namespace SQuick;
 
-abstract class RootDB implements \ArrayAccess, DB\ResultRow{
+abstract class RootDB extends DataObj implements DB\ResultRow{
 	
 	protected $_table = null;
 	protected $_primaryKey = null;
 	protected $_tableInfo = null;
-	protected $_data = null;
 	protected $_originalData = null;
 	protected $_isNew = true;
 	protected $_id = null;
@@ -115,44 +114,6 @@ abstract class RootDB implements \ArrayAccess, DB\ResultRow{
 
 	}
 	
-	public function __set( $key, $value ){
-		if (!array_key_exists( $key, $this->_data )) die("Invalid $key. Does not exist in data");
-		if (!array_key_exists( $key, $this->_tableInfo )) die("Invalid $key. Does not exist in data");
-		
-		//If a _setVarName method exists run that
-		$methodName = "_set{$value}";
-		if ( method_exists( $this, $methodName ) ){
-			$cleanValue = $this->$methodName( $value );
-		}
-		//@TODO Cast the value based on what's expected in the db struction
-		else{
-			// 
-			$cleanValue = $value;
-		}
-		
-		$this->_data[ $key ] = $cleanValue;
-	}
-	
-	public function __get( $key ){
-		if (!array_key_exists( $key, $this->_data )) 
-			throw new DataException("Invalid $key. Does not exist in data");
-
-		return $this->_data[ $key ];
-	}
-
-	public function importArray( $data, $useSetters = true ){
-		
-		foreach ( $data as $column => $value ){
-			if ( $useSetters ){
-				$this->$column = $value;
-			}
-
-			elseif ( array_key_exists( $column, $this->_data) ){
-				$this->_data[$column] = $value;
-			}
-		}
-	}
-
 	/**
 	 * Returns a generic simple form with all the fields as set by the table structure
 	 * @return SQuickForm();
@@ -183,7 +144,7 @@ abstract class RootDB implements \ArrayAccess, DB\ResultRow{
 					break;
 			}
 
-			$ff = new SQuickFormField( $key, $isRequired, $type, $min, $max );
+			$ff = new sq\FormField( $key, $isRequired, $type, $min, $max );
 			$ff->value = $this->$key;
 			$form->addField( $ff );
 		}
@@ -191,15 +152,7 @@ abstract class RootDB implements \ArrayAccess, DB\ResultRow{
 		return $form;
 	}
 
-	public function importFromArray( $array ){
-		
-		foreach ( $this->_data as $key => $data ){
-			if ( array_key_exists( $key, $array ) ){
-				$this->_data[ $key ] = $array[ $key ];
-			}
-		}
 
-	}
 
 	/**
 	 * Fetches an item - from cache.
@@ -230,25 +183,6 @@ abstract class RootDB implements \ArrayAccess, DB\ResultRow{
 		return self::$_dbInstance;
 	}
 
-	public function offsetExists( $key ){
-		return array_key_exists( $key, $this->_normal );
-	}
-
-	public function offsetGet( $key ){
-		$key = 'normal_'.strtolower( $key );
-
-		if ( method_exists( $this, $key) ){
-			return call_user_func( array($this, $key) );
-		}
-	}
-
-	public function offsetSet( $key, $value ){
-
-	}
-
-	public function offsetUnset( $key ){
-
-	}
 
 	public function importSQuickDBResultRow( $row ){
 		$this->importFromArray( $row );
